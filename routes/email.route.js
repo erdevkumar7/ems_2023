0831532053c289f3a59f8361ff1c4c211696330b
devendra <you@example.com>
2023-11-07 19:46:24 +0530

    start sending messages

--- routes/email.route.js
+++ routes/email.route.js
@@ -2,13 +2,14 @@ const express = require("express");
 // const { upload } = require("../helper/upload");
 const {
     sendEmail,
-    getEmailById
+    getEmailById, getEmailByUserId
 } = require("../controllers/email.controller");
 // const { webProtection } = require("../helper/auth");
 const router = express.Router();
 
 router.post('/sendemail', sendEmail)
 router.get("/getemailbyid/:id", getEmailById)
+router.get('/getemailbyuserid/:id', getEmailByUserId);
 // router.get('/getuser/:id', getUserById)
 // router.post('/loginuser' ,loginUser)
 // router.put('/updateuser/:id', upload.single("profile_pic") , updateUser)
